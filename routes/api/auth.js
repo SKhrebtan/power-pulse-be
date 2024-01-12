0016262f4dc4b6e8b3b850e855fa01b03ebf3a49
Alyona <polova_alyona0907@ukr.net>
2024-01-12 22:03:06 +0100

    login, logout, current, update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const authCtrl = require('../../controllers/auth');
+const authenticate = require('../../middlewares/authenticate');
 const validateBody = require('../../middlewares/validateBody');
-const { registerSchema } = require('../../models/user');
+const { registerSchema, loginSchema } = require('../../models/user');
 
 const router = express.Router();
 
 router.post('/register', validateBody(registerSchema), authCtrl.register);
-
+router.post('/login', validateBody(loginSchema), authCtrl.login);
+router.get('/current', authenticate, authCtrl.current);
+router.post('/logout', authenticate, authCtrl.logout);
+router.patch('/', authenticate, authCtrl.update);
 module.exports = router;
