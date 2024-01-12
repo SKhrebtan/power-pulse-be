d96bc577ea99edab0ea5b8d2fa1ff5abdfff9ad2
OlehKhv <126176715+OlehKhv@users.noreply.github.com>
2024-01-12 17:18:45 +0200

    remove files

--- helpers/handleResizeAvatar.js
+++
@@ -1,8 +0,0 @@
-const Jimp = require('jimp');
-
-const handleResizeAvatar = async pathToImg => {
-    const img = await Jimp.read(pathToImg);
-    img.resize(250, 250).write(pathToImg);
-};
-
-module.exports = handleResizeAvatar;
