ad17eb1ebaef57887cae50b08a80141d3084132f
OlehKhv <126176715+OlehKhv@users.noreply.github.com>
2024-01-12 17:51:19 +0200

    fix route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRouter = require('./routes/api/auth');
-// const contactsRouter = require('./routes/api/contacts');
 
 const app = express();
 
@@ -15,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/users', authRouter);
+app.use('/api/v1/users', authRouter);
 // app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
