348ca7855d11820e2f3c3ae032c3b516affc9f03
nicksolony <nicksolony@gmail.com>
2024-01-13 22:15:03 -0500

    setup local db and started creating diaryrecord model

---
+++ models/diaryRecord.js
@@ -0,0 +1,87 @@
+const { Schema, model } = require('mongoose');
+const Joi = require("joi");
+const { handleMongooseError } = require('../helpers');
+
+const datePattern = /^\d{2}-\d{2}-\d{4}$/
+
+const productRecordSchema = new Schema(
+    {
+        product: {
+            type: Schema.Types.ObjectID,
+            ref: 'product',
+            required: [true, 'Product ID is required'],
+        },
+        amount: {
+            type: Number,
+            required: [true, 'Amount is required'],
+            min: [1, 'Amount must be at least 1 gram'],
+        },
+        calories: {
+            type: Number,
+            required: [true, 'Calories are required'],
+            min: [1, 'Must record at least 1 calorie'],
+        },
+    },
+);
+
+const exerciseRecordSchema = new Schema(
+    {
+        exercise: {
+            type: Schema.Types.ObjectID,
+            ref: 'exercise',
+            required: [true, 'Exercise ID is required'],
+        },
+        time: {
+            type: Number,
+            required: [true, 'Time is required'],
+            min: [1, 'Time must be at least 1 minute'],
+        },
+        calories: {
+            type: Number,
+            required: [true, 'Calories are required'],
+            min: [1, 'Must record at least 1 calorie burned'],
+        },
+    },
+);
+
+const diaryRecordSchema = new Schema(
+    {
+        date: {
+            type: String,
+            require: true,
+            match: datePattern,
+        },
+        user: {
+            type: Schema.Types.ObjectID,
+            ref: 'user',
+            required: [true, 'User ID is required'],
+        },
+        products: [productRecordSchema],
+        exercises: [exerciseRecordSchema],
+        // caloriesConsumed: {
+        //     type: Number,
+        //     required: true,
+        //     default: 0,
+        // },
+        // caloriesBurned: {
+        //     type: Number,
+        //     required: true,
+        //     default: 0,
+        // },
+        // activity: {
+        //     type: Number,
+        //     required: true,
+        //     default: 0,
+        // },
+    },
+);
+
+diaryRecordSchema.post('save', handleMongooseError);
+
+const addDiaryProductSchema = Joi.object({
+
+});
+
+const DiaryRecord = model('diaryRecord', diaryRecordSchema);
+
+module.exports = { DiaryRecord };
---
+++ routes/api/diaryRecords.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+
+// const ctrl = require('../../controllers/diaryRecords');
+// const { validateBody, isValidId, authenticate } = require('../../middlewares');
+
+// const router = express.Router();
+
+
+// // add product to diary
+// router.post('/:productId', isValidId, authenticate, validateBody(schemas.addDiaryProduct), ctrl.addDiaryProduct);
+
+// module.exports = router;
