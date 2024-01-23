d5b98d8bde495c05a75c0cd6fa7b178efdc173a3
OlehKhv <126176715+OlehKhv@users.noreply.github.com>
2024-01-23 22:52:36 +0200

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ const register = async (req, res) => {
                   <td class="es-m-p0r" valign="top" align="center" style="padding:0;Margin:0;width:560px">
                    <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                      <tr>
-                      <td align="center" style="padding:0;Margin:0;padding-top:10px;padding-bottom:10px;font-size:0px"><img src="https://power-4vwy.onrender.com/logo/logo.png" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="100"></td>
+                      <td align="center" style="padding:0;Margin:0;padding-top:10px;padding-bottom:10px;font-size:0px"><img src="https://power-4vwy.onrender.com/logo/logo.png" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="200"></td>
                      </tr>
                    </table></td>
                  </tr>
